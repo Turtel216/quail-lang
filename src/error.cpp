62eb731c761ea9434f5b0917c885db030583f9e5
Dimitrios Papakonstantinou <papakonstantinou.dm@gmail.com>
2026-03-02 13:54:50 +0200

    Fix spelling in error message

--- src/error.cpp
+++ src/error.cpp
@@ -3,7 +3,7 @@
 namespace ff {
 
 const char *TypeError::what() const noexcept {
-  return "an error occured type checking";
+  return "an error occured while type checking";
 }
 
 } // namespace ff
