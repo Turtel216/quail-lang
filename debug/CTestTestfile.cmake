f61329efd9c3f342864d5b6a5465cf525a4548f0
Dimitrios Papakonstantinou <papakonstantinou.dm@gmail.com>
2026-03-04 00:55:04 +0200

    Remove debug directory

--- debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dimitrios/Documents/repos/ff
-# Build directory: /home/dimitrios/Documents/repos/ff/debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
